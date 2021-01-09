74e3ed90b97ad4a3fc3b63930c2b231c7bddd6a0
sobhanthakur <sobhant@mindfiresolutions.com>
2021-01-09 18:07:59 +0530

    Add google cloud speech to convert audio to text

--- api/controllers/Filter.js
+++ api/controllers/Filter.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const filterService = require("../services/Filter");
-var multer  = require('multer');
-var upload = multer({ dest: './uploads/' })
+var multer = require("multer");
+var upload = multer({ dest: "./uploads/" });
 
 // @route    POST api/filter
 // @desc     Filter Audio
 // @access   Public
 
-router.post(
-  "/",
-  upload.single('Video'),
-  (req, res) => {
-    filterService.VideoToAudio(req,res);
-    return res.status(201).json({msg:"Coming"});
-  }
-);
+router.post("/", upload.single("Video"), async (req, res) => {
+  const audio = await filterService.VideoToAudio(req, res);
+  console.log(audio);
+  const text = await filterService.AudioToSpeech(audio);
+  return res.status(201).json({ msg: text });
+});
 
 module.exports = router;
--- api/services/Filter.js
+++ api/services/Filter.js
@@ -1,20 +1,56 @@
-var ffmpeg = require('fluent-ffmpeg');
-var fs = require('fs');
+var ffmpeg = require("fluent-ffmpeg");
+var fs = require("fs");
+const speech = require("@google-cloud/speech");
 
 /*
  * Convert Video to Audio
  */
 const VideoToAudio = async (req, res) => {
-  console.log(req.file);
+  //   console.log(req.file);
+  return new Promise((resolve, reject) => {
     convert(
       req.file.path,
       req.file.destination + "output.flac",
       function (err) {
         if (!err) {
-          fs.unlinkSync(req.file.path)
+          fs.unlinkSync(req.file.path);
+          resolve(req.file.destination + "output.flac");
+        } else {
+          reject(err);
         }
       }
     );
+  });
+};
+
+const AudioToSpeech = async (filename) => {
+  const client = new speech.SpeechClient();
+
+  const file = fs.readFileSync(filename);
+  const audioBytes = file.toString("base64");
+
+  const audio = {
+    content: audioBytes,
+  };
+  const config = {
+    encoding: "LINEAR16",
+    sampleRateHertz: 16000,
+    languageCode: "en-US",
+  };
+
+  const request = {
+    audio: audio,
+    config: config,
+  };
+
+  const [response] = await client.recognize(request);
+
+  const transcription = response.results
+    .map((result) => result.alternatives[0].transcript)
+    .join("\n");
+  console.log(`Transcription: ${transcription}`);
+
+  return transcription;
 };
 
 /**
@@ -22,8 +58,16 @@ const VideoToAudio = async (req, res) => {
  *    output - string, path of output file
  *    callback - function, node-style callback fn (error, result)
  */
-function convert(input, output, callback) {
+const convert = async (input, output, callback) => {
   ffmpeg(input)
+    .outputOptions([
+      "-f s16le",
+      "-acodec pcm_s16le",
+      "-vn",
+      "-ac 1",
+      "-ar 16k",
+      "-map_metadata -1",
+    ])
     .output(output)
     .on("end", function () {
       console.log("conversion ended");
@@ -34,6 +78,6 @@ function convert(input, output, callback) {
       callback(err);
     })
     .run();
-}
+};
 
-module.exports = { VideoToAudio };
+module.exports = { AudioToSpeech, VideoToAudio };
